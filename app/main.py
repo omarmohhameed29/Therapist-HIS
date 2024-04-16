dcd455a614a1b637f32a9a188d7bd11ab48db3b5
Amir Hesham Ibrahim <amirhesham65@gmail.com>
2024-04-16 22:27:13 +0200

    Add CORS support

--- app/main.py
+++ app/main.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import patient, therapist, receptionist
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(patient.router)
 app.include_router(therapist.router)
 app.include_router(receptionist.router)
-
